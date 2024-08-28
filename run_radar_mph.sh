e8ac2a352f94e5732e34e8c1eaeb9f9e8c62004f
learn-as-i-go <magicofray@gmail.com>
2024-08-28 11:39:03 -0400

    Add files via upload

---
+++ run_radar_mph.sh
@@ -0,0 +1,19 @@
+#!/bin/bash
+
+echo "script started at $(date)" >> /home/pi/OPS241A_RasPiLCD/cron-log.txt
+
+#set environment variables (adjust as needed)
+export PATH=/usr/local/bin:/usr/bin:/bin:/usr/sbin:home/pi/.local/bin
+export PYTHONPATH=/usr/lib/python3/dist-packages
+
+echo "Environment variables set" >> /home/pi/OPS241A_RasPiLCD/cron-log.txt
+
+#navigate to project directory
+cd /home/pi/OPS241A_RasPiLCD
+
+echo "Directory changed to $(pwd)" >> /home/pi/OPS241A_RasPiLCD/cron-log.txt
+
+#run script
+sudo /usr/bin/python3 /home/pi/OPS241A_RasPiLCD/speed-mph-v2.py > /home/pi/OPS241A_RasPiLCD/speed-mph-v2.log 2>&1
+
+echo "python script executed" >> /home/pi/OPS241A_RasPiLCD/cron-log.txt
--- speed.py
+++ speed.py
@@ -12,11 +12,10 @@ from pygame.locals import *
 from datetime import datetime, timedelta
 
 # Ops241A module settings:  ftps, dir off, 5Ksps, min -9dB pwr, squelch 5000
-Ops241A_Speed_Output_Units = 'US'
-Ops241A_Speed_Output_Units_lbl = 'mph'
-OPS_current_units = 0
+Ops241A_Speed_Output_Units = ['US','UK','UM','UC']
+Ops241A_Speed_Output_Units_lbl = ['mph','km/h','m/s','cm/s']
+OPS_current_units = 3
 Ops241A_Blanks_Pref_Zero = 'BZ'
-Ops241A_Min_Reported_Speed = 'R>5\n'
 Ops241A_Sampling_Frequency = 'SV'
 Ops241A_Transmit_Power = 'PD'    # miD power
 Ops241A_Threshold_Control = 'MX' # 1000 magnitude-square.  10 as reported
@@ -69,7 +68,7 @@ speed_col = int(screen_size[0] / 4)  # quarter of the way in
 speed_row = logo_height + int(speed_font_size * 0.3)  # nudge a bit
 
 units_lbl_font = pygame.font.SysFont(speed_font_name, units_lbl_font_size, True, False)
-units_lbl = units_lbl_font.render("m/s", True, WHITE)    
+units_lbl = units_lbl_font.render("m/s", True, WHITE)
 units_lbl_col = int(3*(screen_size[0] / 4))  # three quarter of the way in
 units_lbl_row = (speed_row + speed_font_size) - (2*units_lbl_font_size)
 screen.blit(units_lbl, [units_lbl_col, units_lbl_row])
@@ -107,17 +106,16 @@ def send_serial_cmd(print_prefix, command) :
             data_rx_str = str(data_rx_bytes)
             if data_rx_str.find(ser_message_start) :
                 ser_write_verify = True
-            
+
 # Initialize and query Ops241A Module
 print("\nInitializing Ops241A Module")
-send_serial_cmd("\nSet Speed Output Units: ", Ops241A_Speed_Output_Units)
-units_lbl = units_lbl_font.render(Ops241A_Speed_Output_Units_lbl, True, WHITE)    
+send_serial_cmd("\nSet Speed Output Units: ", Ops241A_Speed_Output_Units[OPS_current_units])
+units_lbl = units_lbl_font.render(Ops241A_Speed_Output_Units_lbl[OPS_current_units], True, WHITE)
 send_serial_cmd("\nSet Sampling Frequency: ", Ops241A_Sampling_Frequency)
 send_serial_cmd("\nSet Transmit Power: ", Ops241A_Transmit_Power)
 send_serial_cmd("\nSet Threshold Control: ", Ops241A_Threshold_Control)
 #send_serial_cmd("\nModule Information: ", Ops241A_Module_Information)
-send_serial_cmd("\nSet Blanks Preference: ", Ops241A_Blanks_Pref_Zero) 
-send_serial_cmd("\nSet Reported MinSpeed: ", Ops241A_Min_Reported_Speed,"\n") 
+send_serial_cmd("\nSet Blanks Preference: ", Ops241A_Blanks_Pref_Zero)
 #send_serial_cmd("\nModule Information: ", Ops241A_Module_Information)
 
 
@@ -145,18 +143,18 @@ while not done:
     if speed_available == True :
         pygame.draw.rect(
             screen,
-            screen_bkgnd_color, 
+            screen_bkgnd_color,
             (speed_col,speed_row,screen_size_width-speed_col,speed_font_size),
             0)
         # Render the text for display. "True" means anti-aliased text.
         speed_rnd = round(Ops241_rx_float, 1)
         speed_str = str(speed_rnd)
         if speed_rnd < 0:
-            speed_rend = speed_font.render(speed_str, True, WHITE)    
+            speed_rend = speed_font.render(speed_str, True, WHITE)
         elif speed_rnd > 0:
-            speed_rend = speed_font.render(speed_str, True, RED)    
+            speed_rend = speed_font.render(speed_str, True, RED)
         else:
-            speed_rend = speed_font.render(speed_str, True, WHITE)    
+            speed_rend = speed_font.render(speed_str, True, WHITE)
         screen.blit(speed_rend, [speed_col, speed_row])
         screen.blit(units_lbl, [units_lbl_col, units_lbl_row])
 
@@ -164,8 +162,17 @@ while not done:
         pygame.display.flip()
         # Limit to 60 frames per second
 
+    now = datetime.now()
+    if (last_units_change + interval_timedelta) < now:
+        if OPS_current_units == len(Ops241A_Speed_Output_Units)-1:
+            OPS_current_units=0
+        else:
+            OPS_current_units+=1
+        send_serial_cmd("\nSet Speed Output Units: ", Ops241A_Speed_Output_Units[OPS_current_units])
+        units_lbl = units_lbl_font.render(Ops241A_Speed_Output_Units_lbl[OPS_current_units], True, WHITE)
+        last_units_change = now
+
     for event in pygame.event.get():
         if event.type == pygame.QUIT:
             pygame.quit()
             exit()
-            
